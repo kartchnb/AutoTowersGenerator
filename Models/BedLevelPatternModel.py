f034f7ffffa3d193443a7b3c23878ad7c061958d
5axes <5axes@users.noreply.github.com>
2023-08-24 15:45:11 +0200

    Add Ident for bed level
    need to keep the original type to generate the OpenScadModel

--- Models/BedLevelPatternModel.py
+++ Models/BedLevelPatternModel.py
@@ -29,16 +29,15 @@ class BedLevelPatternModel(ModelBase):
 
     # The available bed level patterns
     _patternsTable = [
-        {'name': catalog.i18nc("@pattern", "Spiral Squares") , 'icon': 'bedlevelpattern_spiral_squares_icon.png'}, 
-        {'name': catalog.i18nc("@pattern", "Concentric Squares") , 'icon': 'bedlevelpattern_concentric_squares_icon.png'}, 
-        {'name': catalog.i18nc("@pattern", "Concentric Circles") , 'icon': 'bedlevelpattern_concentric_circles_icon.png'},
-        {'name': catalog.i18nc("@pattern", "X in Square") , 'icon': 'bedlevelpattern_x_in_square_icon.png'}, 
-        {'name': catalog.i18nc("@pattern", "Circle in Square") , 'icon': 'bedlevelpattern_circle_in_square_icon.png'}, 
-        {'name': catalog.i18nc("@pattern", "Grid") , 'icon': 'bedlevelpattern_grid_icon.png'}, 
-        {'name': catalog.i18nc("@pattern", "Padded Grid") , 'icon': 'bedlevelpattern_padded_grid_icon.png'},
-        {'name': catalog.i18nc("@pattern", "Five Circles") , 'icon': 'bedlevelpattern_five_circles_icon.png'}, 
+        {'ident': 'Spiral Squares' , 'name': catalog.i18nc("@pattern", "Spiral Squares") , 'icon': 'bedlevelpattern_spiral_squares_icon.png'}, 
+        {'ident': 'Concentric Squares' , 'name': catalog.i18nc("@pattern", "Concentric Squares") , 'icon': 'bedlevelpattern_concentric_squares_icon.png'}, 
+        {'ident': 'Concentric Circles' , 'name': catalog.i18nc("@pattern", "Concentric Circles") , 'icon': 'bedlevelpattern_concentric_circles_icon.png'},
+        {'ident': 'X in Square' , 'name': catalog.i18nc("@pattern", "X in Square") , 'icon': 'bedlevelpattern_x_in_square_icon.png'}, 
+        {'ident': 'Circle in Square' , 'name': catalog.i18nc("@pattern", "Circle in Square") , 'icon': 'bedlevelpattern_circle_in_square_icon.png'}, 
+        {'ident': 'Grid' , 'name': catalog.i18nc("@pattern", "Grid") , 'icon': 'bedlevelpattern_grid_icon.png'}, 
+        {'ident': 'Padded Grid' , 'name': catalog.i18nc("@pattern", "Padded Grid") , 'icon': 'bedlevelpattern_padded_grid_icon.png'},
+        {'ident': 'Five Circles' , 'name': catalog.i18nc("@pattern", "Five Circles") , 'icon': 'bedlevelpattern_five_circles_icon.png'}, 
     ]
-
 
 
     # Make the presets available to QML
@@ -110,7 +109,7 @@ class BedLevelPatternModel(ModelBase):
 
     @pyqtProperty(str, notify=patternIndexChanged)
     def patternName(self)->str:
-        return self._patternsTable[self.patternIndex]['name']
+        return self._patternsTable[self.patternIndex]['ident']
 
     @pyqtProperty(str, notify=patternIndexChanged)
     def patternIcon(self)->str:
@@ -185,7 +184,6 @@ class BedLevelPatternModel(ModelBase):
     def cellSize(self)->int:
         return int(self.cellSizeStr)
     
-
 
     # The selected pad size
     _padSizeStr = '20'
